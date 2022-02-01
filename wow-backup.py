652da32208513d4daa6d8f23a40cea8788a60873
Alan Scott <alan.edward.scott@gmail.com>
2022-01-31 19:00:55 -0600

    add rename interface functionality

--- wow-backup.py
+++ wow-backup.py
@@ -1,5 +1,10 @@
-import tarfile
+import tarfile, os, re
 from datetime import datetime
+from os import path
+from colorama import Fore
+from colorama import Style
+
+global debug_status
 
 # World of Warcraft root directory
 root = '/mnt/c/Program Files (x86)/World of Warcraft/'
@@ -9,87 +14,187 @@ output = '/mnt/c/Users/alane/OneDrive/Documents/WoW BackUps'
 
 # game versions
 versions = {
-    'Retail': 'retail',
-    'Vanilla': 'classic_era',
-    'TBC': 'classic'
+    'Retail': '_retail_',
+    'Vanilla': '_classic_era_',
+    'TBC': '_classic_'
 }
 
 # menu
 menu_options = {
-    '1': 'WTF',
-    '2': 'WTF & Interface',
+    '1': 'Archive WTF',
+    '2': 'Archive WTF & Interface',
+    '3': 'Rename Interface',
     'q': 'Quit'
 }
 
+menu_options_name = {
+    '1': 'Retail',
+    '2': 'Vanilla',
+    '3': 'TBC',
+    'q': 'Exit Rename'
+}
 
-def do_compression(show_header=False):
+
+def run_manager(show_header=False):
     if show_header:
-        print("World of Warcraft Interface BackUp System\n")
+        print("World of Warcraft Interface Management System\n")
 
-    render_menu()
+    debug_status = True
+    debug_label = f"{Fore.GREEN}ON{Style.RESET_ALL}"
+    if input('Debug Mode? (y/n) [y] ') == 'n':
+        debug_status = False
+        debug_label = f"{Fore.RED}OFF{Style.RESET_ALL}"
+
+    print(f"Debug Mode is {debug_label}\n\n")
+
+    print(build_menu(menu_options))
     action = input('Select Action: ')
 
     if action in menu_options.keys():
         if action == 'q':
             quit()
 
-        directories = build_directory_list()
-        if action != '1':
-            directories_interface = build_directory_list('Interface')
-            directories = directories + directories_interface
+        if action == '3':
+            print(f"\n{build_menu(menu_options_name)}")
+            action = input('Select Reset Action: ')
 
-        do_backup(directories)
+            if action == 'q':
+                print("\n")
+                run_manager()
+            
+            do_interface_rename(versions[menu_options_name[action]], debug_status)
+            run_manager()
+        
+        if action == '4':
+            do_interface_restore(debug_status)
+
+        folders=['WTF']
+        if action != '1':
+            folders.append('Interface')
+        
+        directories = {}
+        directories_failed = {}
+
+        for version in versions:
+            for folder in folders:
+                paths = build_paths(versions[version],folder)
+
+                if os.path.exists(paths['path_folder']):
+                    directories[paths['path_folder']] = paths['path_rel']
+                else:
+                    directories_failed.append(paths['path_folder'])
+
+        if directories:
+            print_summary("Folders added to archive", directories)
+        
+        if directories_failed:
+            print_summary("Folders NOT added to archive (path is invalid)", directories_failed)
+
+        do_interface_archive(directories, debug_status)
 
         print("Backup complete!\n\n")
-        do_compression()
+        run_manager()
     else:
         print("Invalid Selection!\n\n")
-        do_compression()
+        run_manager()
 
 
-def build_directory_list(folder='WTF'):
-    directories = {}
+def do_interface_archive(directories, debug_status=True):
+    filename = f"{output}/{get_datetime()}.tar.gz"
 
-    for label, version in versions.items():
-        path = root + '_' + version + '_' + '/' + folder
-        path_rel = version + '/' + folder
-        directories[path] = path_rel
+    if debug_status == False:
+        with tarfile.open(filename, 'w') as archive:
+            print(f"Creating archive {filename} ...")
 
-        print('Adding ' + folder + ' folder for ' + label + ' to the archive ... ' + path)
+            for value in directories:
+                if os.path.exists(value):
+                    archive.add(value, arcname=directories[value])
 
-    return directories
+        archive.close()
 
 
-def do_backup(directories):
+def do_interface_rename(version, debug_status=True):
+    folders = ['WTF','Interface']
+    filename = get_datetime()
+    renames = {}
+    paths_new = []
+
+    action = re.sub(r'\W+', '', input(f"Interface Name? [{filename}] "))
+
+    if action:
+        filename = action[:35]
+
+    path_archives = os.path.join(root,version,'archives')
+    if os.path.exists(path_archives) == False:
+        print_summary("Create /archives directory", {path_archives})
+
+        if debug_status == False:
+            print('mkdir goes here')
+            #os.mkdir(path_archives)
+
+    for folder in folders:
+        paths = build_paths(version,folder)
+        path_new = os.path.join(path_archives, f"{filename}-{folder}")
+
+        if os.path.exists(paths['path_folder']):
+            if os.path.exists(path_new):
+                print(f"Could not rename {paths['path_rel']} to {path_new} because the folder already exists. Process aborted.\n")
+                run_manager()
+            else:
+                renames[paths['path_folder']] = path_new
+                paths_new.append(path_new)
+        else:
+            print(f"Could not rename {paths['path_rel']} because the path to the folder was invalid! Process aborted.\n")
+            run_manager()
+
+    if renames:
+        print_summary("Interface folders moved to archives", paths_new)
+
+        for k in renames:
+            if debug_status == False:
+                print('rename goes here')
+                #os.rename(k,renames[k])
+
+        print('Rename process complete!\n')
+
+
+def do_interface_restore(debug_status=True):
+    #TODO: check if /archives folder exists for the _version_
+    #TODO: print a list of folders in the /archives folder grouped by timestamp with timestamp converted to y-m-d h:ma format
+    #TODO: run archive for current WTF/Interface in _version_
+    #TODO: rename /archives folders to _version_ folders
+    print('\nNYI! Goodbye!')
+    run_manager()
+
+
+def get_datetime():
     now = datetime.now()
-    now = now.strftime("%Y%m%d%H%M%S")
-
-    filename = output + '/' + now + '.tar.gz'
-
-    with tarfile.open(filename, 'w') as archive:
-        print('Creating archive ' + filename + ' ... ')
-
-        for path in directories:
-            archive.add(path, arcname=directories[path])
-
-    archive.close()
+    return now.strftime("%Y%m%d%H%M%S")
 
 
-def get_menu_options():
-    return {
-        '1': 'WTF',
-        '2': 'WTF & Interface',
-        'q': 'Quit'
-    }
+def print_summary(header,collection):
+    merged_collection = "\n    ".join(collection)
+    print(f"{header}:\n    {merged_collection}")
 
 
-def render_menu():
+def build_menu(options):
     menu = []
 
-    for k, v in menu_options.items():
-        menu.append('[' + k + '] ' + v)
+    for k, v in options.items():
+        menu.append(f"[{k}] {v}")
+    
+    return "\n".join(menu)
 
-    print("\n".join(menu))
 
+def build_paths(version, folder='WTF'):
+    path_version = os.path.join(root,version)
+    path_folder = os.path.join(path_version, folder)
+    path_rel = f"{version}/{folder}"
 
-do_compression(True)
+    return {
+        'path_version': path_version,
+        'path_folder': path_folder,
+        'path_rel': path_rel
+    }
+
+run_manager(True)
