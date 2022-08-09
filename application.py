d6cf8ed69e928d405f9bc9b2ba4436a4b6ec0108
Alan Scott <alan.edward.scott@gmail.com>
2022-08-09 13:39:24 -0500

    add alternate path for root folder

--- application.py
+++ application.py
@@ -7,7 +7,8 @@ from colorama import Style
 global debug_status
 
 # World of Warcraft root directory
-root = '/mnt/c/Program Files (x86)/World of Warcraft/'
+#root = '/mnt/c/Program Files (x86)/World of Warcraft/'
+root = '/mnt/d/World of Warcraft/'
 
 # path to output directory
 output = '/mnt/c/Users/alane/OneDrive/Documents/WoW BackUps'
