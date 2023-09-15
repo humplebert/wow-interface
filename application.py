3cb796f60c98922749577777e2e5318fab3c9f94
Alan Scott <alan.edward.scott@gmail.com>
2023-09-15 15:53:33 -0500

    refined configuration file structure, added support for wow root and output directories

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-import tarfile, os, re, shutil
+import tarfile, os, re, shutil, json
 from datetime import datetime
 from os import path
 from colorama import Fore
@@ -12,7 +12,14 @@ global debug_status
 #root = '/mnt/d/Blizzard/World of Warcraft/'
 
 # path to output directory
-output = '/mnt/c/Users/alane/OneDrive/Documents/WoW BackUps'
+# path_output = '/mnt/c/Users/alane/OneDrive/Documents/WoW BackUps'
+# path_output = '/mnt/d/My Drive/Applications/WoW BackUps'
+path_configuration = '/home/humplebert/wowi-configuration.json'
+
+template_configuration = {
+    'path_wow_root': '',
+    'path_output': '',
+}
 
 # game versions
 versions = {
@@ -27,6 +34,7 @@ menu_options = {
     '2': 'Archive WTF & Interface',
     '3': 'Rename Interface',
     '4': 'Restore Interface',
+    'p': 'Update Paths',
     'q': 'Quit'
 }
 
@@ -37,15 +45,39 @@ menu_options_versions = {
     'q': 'Quit'
 }
 
+menu_options_path_wow_root = {
+    '1': '/mnt/c/Program Files (x86)/World of Warcraft/',
+    '2': '/mnt/c/Blizzard/World of Warcraft/',
+    '3': '/mnt/d/Blizzard/World of Warcraft/',
+    'o': 'Other (manually define)',
+    'q': 'Quit'
+}
+
+menu_options_path_output = {
+    '1': '/mnt/d/My Drive/Applications/WoW BackUps',
+    '2': '/mnt/c/Users/alane/OneDrive/Documents/WoW BackUps',
+    'o': 'Other (manually define)',
+    'q': 'Quit'
+}
 
 def run_manager(show_header=False):
     if show_header:
-        print("World of Warcraft Interface Management System\n")
+        print("World of Warcraft Interface Management System")
+        print_configuration('path_wow_root','Current Root Path')
+        print_configuration('path_output','Current BackUp Path')
 
     debug_status = True
     debug_label = f"{Fore.GREEN}ON{Style.RESET_ALL}"
 
-    root = rootpath()
+    if not bool(get_configuration('path_wow_root')):
+        set_path(menu_options_path_wow_root, 'Select path to World of Warcraft root directory... ', 'path_wow_root')
+        run_manager(True)
+
+    if not bool(get_configuration('path_output')):
+        set_path(menu_options_path_output, 'Select path to WoW BackUps output directory... ', 'path_output')
+        run_manager(True)
+
+    path_wow_root = get_configuration('path_wow_root')
 
     # set Debug Mode
     action = input('Debug Mode? (y/n/q) [y] ')
@@ -64,6 +96,11 @@ def run_manager(show_header=False):
     if action in menu_options.keys():
         exit_program(action)
 
+        if action == 'p':
+            set_path(menu_options_path_wow_root, 'Select path to World of Warcraft root directory... ', 'path_wow_root')
+            set_path(menu_options_path_output, 'Select path to WoW BackUps output directory... ', 'path_output')
+            run_manager(True)
+
         if action == '3':
             print(f"\n{build_menu(menu_options_versions)}")
             action = input('Select Rename Action: ')
@@ -72,11 +109,11 @@ def run_manager(show_header=False):
                 print("\n")
                 run_manager()
 
-            do_interface_rename(versions[menu_options_versions[action]], root, debug_status)
+            do_interface_rename(versions[menu_options_versions[action]], path_wow_root, debug_status)
             run_manager()
 
         if action == '4':
-            do_interface_restore(debug_status, root)
+            do_interface_restore(debug_status, path_wow_root)
 
         folders=['WTF']
         if action != '1':
@@ -87,7 +124,7 @@ def run_manager(show_header=False):
 
         for version in versions:
             for folder in folders:
-                paths = build_paths(versions[version], root, folder)
+                paths = build_paths(versions[version], path_wow_root, folder)
 
                 if os.path.exists(paths['path_folder']):
                     directories[paths['path_folder']] = paths['path_rel']
@@ -102,10 +139,10 @@ def run_manager(show_header=False):
 
         do_interface_archive(directories, debug_status)
 
-        print("Backup complete!\n\n")
+        print(f"{Fore.GREEN}Backup complete!{Style.RESET_ALL}\n\n")
         run_manager()
     else:
-        print("Invalid Selection!\n\n")
+        print(f"{Fore.RED}Invalid Selection!{Style.RESET_ALL}\n\n")
         run_manager()
 
 
@@ -114,11 +151,12 @@ def exit_program(action):
         quit()
 
 def do_interface_archive(directories, debug_status=True):
-    filename = f"{output}/{get_datetime()}.tar.gz"
+    path_output = get_configuration('path_output')
+    filename = f"{path_output}/{get_datetime()}.tar.gz"
+    print(f"Creating archive {Fore.CYAN}{filename}{Style.RESET_ALL} ...")
 
     if debug_status == False:
         with tarfile.open(filename, 'w') as archive:
-            print(f"Creating archive {filename} ...")
 
             for value in directories:
                 if os.path.exists(value):
@@ -214,36 +252,58 @@ def build_paths(version, root, folder='WTF'):
         'path_rel': path_rel
     }
 
-def check_rootpath():
-    return os.path.exists('rootpath.txt')
+def check_path_configuration():
+    if not bool(os.path.exists(path_configuration)):
+        write_configuration_file(template_configuration)
 
-def read_rootpath():
-    if check_rootpath():
-        with open('rootpath.txt', 'r') as f:
-            contents = f.read()
+def check_path_wow_root():
+    if get_configuration('path_wow_root'):
+        return True
 
-    return contents
+    return False
 
-def rootpath():
-    root_path_options = {
-        '1': '/mnt/c/Program Files (x86)/World of Warcraft/',
-        '2': '/mnt/c/Blizzard/World of Warcraft/',
-        '3': '/mnt/d/Blizzard/World of Warcraft/',
-        'q': 'Quit'
-    }
+def write_configuration_file(configuration_json):
+    configuration_file = open(path_configuration, 'w')
+    configuration_file.write(json.dumps(configuration_json))
+    configuration_file.close()
 
-    # read rootpath.txt
-    if check_rootpath() == False:
-        print(f'Select path to World of Warcraft root directory... ')
-        print(build_menu(root_path_options))
-        action = input('Select Path: ')
-        exit_program(action)
-        rootpath = open('rootpath.txt', 'w')
-        rootpath.write(root_path_options[action])
+def get_configuration(configuration_key='ALL'):
+    check_path_configuration()
 
-    root = read_rootpath()
-    print(f"Current Root Path: {Fore.YELLOW}{root}{Style.RESET_ALL}")
+    configuration_file = open(path_configuration,)
+    configuration = json.load(configuration_file)
 
-    return root
+    if configuration_key == 'ALL':
+        return configuration
+
+    if configuration_key in configuration:
+        return configuration[configuration_key]
+
+    return
+
+def print_configuration(configuration_key, label):
+    print(f"{label}: {Fore.YELLOW}{get_configuration(configuration_key)}{Style.RESET_ALL}")
+
+def set_path(menu_options, menu_prompt, configuration_key):
+    check_path_configuration()
+    configuration = get_configuration()
+
+    print(f"{menu_prompt}")
+    print(build_menu(menu_options))
+    action = input('Select Option: ')
+    exit_program(action)
+
+    if 'o' == action:
+        path_value = input('Enter Full Path: ')
+    else:
+        path_value = menu_options[action]
+
+    if not bool(os.path.exists(path_value)):
+        print(f"{Fore.RED}Path '{path_value}' is INVALID!{Style.RESET_ALL}")
+        set_path(menu_options, menu_prompt, configuration_key)
+    else:
+        print(f"{Fore.GREEN}Path is VALID!{Style.RESET_ALL}")
+        configuration[configuration_key] = path_value
+        write_configuration_file(configuration)
 
 run_manager(True)
